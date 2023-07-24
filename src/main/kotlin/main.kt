8510e2bd1154348a1737c8cf78b580ce9f00bb92
Evgeny Zemtsov <evan77@bk.ru>
2023-07-24 19:53:18 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,69 @@
+package ru.netology
+
+const val MasterCard = 1
+const val Maestro = 2
+const val Visa = 3
+const val Mir = 4
+const val VK_pay = 5
+const val limitDayVK = 15_000
+const val limitDayCard = 150_000
+const val limitMonthCard = 600_000
+const val limitMonthVK = 40_000
+
+fun main() {
+    val sumTransfer = 25_000
+    val typeCard = 2
+    val amount = 270_000
+    val commission = transfer(typeCard, amount, sumTransfer)
+    if (commission == "-1") println("Перевод не выполнен, превышены лимиты!")
+    else {
+        println("Коммисия за перевод составила $commission руб.")
+    }
+}
+
+fun transfer(Card: Int = VK_pay, amountAll: Int = 0, sum: Int): String {
+    var success = true
+    var commission = 0.0
+    when (Card) {
+        VK_pay -> {
+            if (!checkLimitVK(sum, amountAll)) success = false
+        }
+
+        MasterCard, Maestro -> {
+            if (checkLimitCard(sum, amountAll)) {
+                if ((sum + amountAll) > 75_000) commission = sum * 0.006 + 20
+            } else success = false
+        }
+
+        Visa, Mir -> {
+            if (checkLimitCard(sum, amountAll)) {
+                commission = sum * 0.0075
+                if (commission < 35) commission = 35.0
+            } else success = false
+        }
+
+        else -> {
+            if (!checkLimitVK(sum, amountAll)) success = false
+        }
+    }
+    val strValue: String = if (!success) {
+        "-1"
+    } else {
+        String.format("%.2f", commission)
+    }
+    return strValue
+}
+
+fun checkLimitCard(sum: Int, amount: Int): Boolean {
+    return when {
+        (sum <= limitDayCard) && (sum + amount <= limitMonthCard) -> true
+        else -> false
+    }
+}
+
+fun checkLimitVK(sum: Int, amount: Int): Boolean {
+    return when {
+        (sum <= limitDayVK) && (sum + amount <= limitMonthVK) -> true
+        else -> false
+    }
+}
---
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -0,0 +1,160 @@
+package ru.netology
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+
+    @Test
+    fun transferMir() {
+        val sumTransfer = 25_000
+        val typeCard = 4                // type card MIR, Visa
+        val amount = 270_000
+
+        val result = transfer(
+            Card = typeCard,
+            amountAll = amount,
+            sum = sumTransfer
+        )
+        assertEquals("187,50", result)
+    }
+
+    @Test
+    fun transferMirMinCom() {
+        val sumTransfer = 2_000
+        val typeCard = 4                // type card MIR, Visa
+        val amount = 270_000
+
+        val result = transfer(
+            Card = typeCard,
+            amountAll = amount,
+            sum = sumTransfer
+        )
+        assertEquals("35,00", result)
+    }
+
+    @Test
+    fun transferMirOutLimit() {
+        val sumTransfer = 225_000
+        val typeCard = 4                // type card MIR, Visa
+        val amount = 270_000
+
+        val result = transfer(
+            Card = typeCard,
+            amountAll = amount,
+            sum = sumTransfer
+        )
+        assertEquals("-1", result)
+    }
+
+    @Test
+    fun transferMaster() {
+        val sumTransfer = 85_000
+        val typeCard = 1                // type card Master, Maestro
+        val amount = 270_000
+
+        val result = transfer(
+            Card = typeCard,
+            amountAll = amount,
+            sum = sumTransfer
+        )
+        assertEquals("530,00", result)
+    }
+
+    @Test
+    fun transferMasterZeroCom() {
+        val sumTransfer = 50_000
+        val typeCard = 1                // type card Master, Maestro
+        val amount = 10_000
+
+        val result = transfer(
+            Card = typeCard,
+            amountAll = amount,
+            sum = sumTransfer
+        )
+        assertEquals("0,00", result)
+    }
+
+    @Test
+    fun transferMasterOutLimit() {
+        val sumTransfer = 85_000
+        val typeCard = 1                // type card Master, Maestro
+        val amount = 550_000
+
+        val result = transfer(
+            Card = typeCard,
+            amountAll = amount,
+            sum = sumTransfer
+        )
+        assertEquals("-1", result)
+    }
+
+    @Test
+    fun transferVK() {
+        val sumTransfer = 15_000
+        val typeCard = 5                // type card VK
+        val amount = 25_000
+
+        val result = transfer(
+            Card = typeCard,
+            amountAll = amount,
+            sum = sumTransfer
+        )
+        assertEquals("0,00", result)
+    }
+
+    @Test
+    fun transferVKOutLimit() {
+        val sumTransfer = 25_000
+        val typeCard = 5                // type card VK
+        val amount = 25_000
+
+        val result = transfer(
+            Card = typeCard,
+            amountAll = amount,
+            sum = sumTransfer
+        )
+        assertEquals("-1", result)
+    }
+
+    @Test
+    fun transferDefaultOutLimit() {
+        val sumTransfer = 5_000
+        val typeCard = 6                // type card Default VK
+        val amount = 40_000
+
+        val result = transfer(
+            Card = typeCard,
+            amountAll = amount,
+            sum = sumTransfer
+        )
+        assertEquals("-1", result)
+    }
+
+    @Test
+    fun transferDefault() {
+        val sumTransfer = 15_000
+        val typeCard = 6                // type card Default VK
+        val amount = 25_000
+
+        val result = transfer(
+            Card = typeCard,
+            amountAll = amount,
+            sum = sumTransfer
+        )
+        assertEquals("0,00", result)
+    }
+
+    @Test
+    fun transferDefaultOutLimit2() {
+        val sumTransfer = 25_000
+        val amount = 55_000
+
+        val result = transfer(
+            amountAll = amount,
+            sum = sumTransfer
+        )
+        assertEquals("-1", result)
+    }
+}
